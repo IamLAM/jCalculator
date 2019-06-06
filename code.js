12dc3bfda107db8fe30d6d10737f7b50a941ce80
IamLAM <ingeniero@luismonge.com.mx>
2019-06-06 15:19:09 -0700

    event click detected

--- code.js
+++ code.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
 
 console.log("jquery vinculado");
 
+$(".buttons").on("click", state);
+
+console.log(action);
+
+function state(){
+    var action= event.currentTarget.innerHTML;
+    console.log(action);
+
+}
+
 });
